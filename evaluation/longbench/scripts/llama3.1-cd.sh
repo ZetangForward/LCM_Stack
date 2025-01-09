088c1b462cdb980862034ffbe7f7870af501de9e
ZetangForward <zecheng.tang@foxmail.com>
2025-01-09 14:36:25 +0800

    dsa

--- evaluation/longbench/scripts/llama3.1-cd.sh
+++ evaluation/longbench/scripts/llama3.1-cd.sh
@@ -1,7 +1,7 @@
 python main.py \
     --model_path="meta-llama/Meta-Llama-3.1-8B-Instruct" \
     --adapter_path="/mnt/petrelfs/tangzecheng/remote_bucket/zecheng/ckpt/merge_v1_fix/Llama-3.1-8B-Instruct/context_denoise/global_step350" \
-    --gpu_lst="1,2,3,6"
+    --gpu_lst="1,2,3,6" \
     --model_max_length_setting="normal_setting" \
     --save_path="./results/Llama3.1";
 
