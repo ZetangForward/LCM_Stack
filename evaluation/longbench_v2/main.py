3d0f75fc35306c61dd3aad2a217a0a639ead352b
ZetangForward <zecheng.tang@foxmail.com>
2025-01-26 01:15:27 +0800

    dsa

--- evaluation/longbench_v2/main.py
+++ evaluation/longbench_v2/main.py
@@ -224,7 +224,7 @@ if __name__ == "__main__":
 
 
     dataset = json.load(open('/data/pub_data/LongBench-v2/data.json', 'r', encoding='utf-8'))
-    data_all = [{"_id": item["_id"], "domain": item["domain"], "sub_domain": item["sub_domain"], "difficulty": item["difficulty"], "length": item["length"], "question": item["question"], "choice_A": item["choice_A"], "choice_B": item["choice_B"], "choice_C": item["choice_C"], "choice_D": item["choice_D"], "answer": item["answer"], "context": item["context"],"index": i} for i, item in enumerate(dataset)]
+    data_all = [{"_id": item["_id"], "domain": item["domain"], "sub_domain": item["sub_domain"], "difficulty": item["difficulty"], "length": item["length"], "question": item["question"], "choice_A": item["choice_A"], "choice_B": item["choice_B"], "choice_C": item["choice_C"], "choice_D": item["choice_D"], "answer": item["answer"], "context": item["context"],"index": i} for i, item in enumerate(dataset) if item["length"].lower() !="long"]
 
 
     test_datasets = ['Code Repository Understanding', 
