7fc7b3435aa941470c7c5217c2639d5709d5df3e
ZetangForward <zecheng.tang@foxmail.com>
2025-01-25 14:35:07 +0800

    dsa

--- evaluation/longbench/scripts/contextual_denoising/llama3.1-8b-base.sh
+++ evaluation/longbench/scripts/contextual_denoising/llama3.1-8b-base.sh
@@ -1,11 +1,8 @@
 python main.py \
-    --model_path="/mnt/hwfile/opendatalab/tangzecheng/local_ckpt/pg19/Llama-3.1-8B/cd_lm_full-0.005/global_step300" \
+    --model_path="/mnt/hwfile/opendatalab/tangzecheng/local_ckpt/merge_v1_w_clues/Llama-3.1-8B/ins_adjust_weight_base_full-0.05/global_step250" \
     --adapter_path="" \
     --gpu_lst="0,1,2,3,4,5,6,7" \
-    --tag="cd_lm_full-0.005" \
+    --tag="ins_adjust_weight_base_full-0.05-250steps" \
     --model_max_length_setting="normal_setting" \
+    --chat_template="User: {}\nAssistant: " \
     --save_path="./results/Llama3.1-8B";
-
-# echo "generate finish ..., begin to evaluate ..."
-
-# python eval_longbench.py --pred_path=${SAVE_PATH}/${MODEL_NAME}
--- evaluation/longbench/scripts/contextual_denoising/llama3.1-8b-test.sh
+++ evaluation/longbench/scripts/contextual_denoising/llama3.1-8b-test.sh
@@ -1,11 +1,8 @@
 python main.py \
-    --model_path="/mnt/hwfile/opendatalab/tangzecheng/local_ckpt/pg19/Llama-3.1-8B/opposite_gradient_large_pos-1e-3/global_step250" \
+    --model_path="/mnt/hwfile/opendatalab/tangzecheng/local_ckpt/pg19/Llama-3.1-8B/opposite_gradient_large_pos-1e-3-v2/global_step300" \
     --adapter_path="" \
     --gpu_lst="0,1,2,3,4,5,6,7" \
-    --tag="opposite_gradient_large_pos-1e-3" \
+    --tag="opposite_gradient_large_pos-1e-3-v2-300steps" \
     --model_max_length_setting="normal_setting" \
+    --chat_template="User: {}\nAssistant: " \
     --save_path="./results/Llama3.1-8B";
-
-# echo "generate finish ..., begin to evaluate ..."
-
-# python eval_longbench.py --pred_path=${SAVE_PATH}/${MODEL_NAME}
